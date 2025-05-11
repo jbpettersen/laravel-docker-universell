696363be7b5e9fa7b553544043f79152cf162d00
Jan Bjerck Pettersen <13648692+jbpettersen@users.noreply.github.com>
2025-05-11 02:56:54 +0200

    Removed Modules cataloge

--- src/Modules/Kalkyle/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-kalkyle::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('kalkyle.name') !!}</p>
-</x-kalkyle::layouts.master>
--- src/Modules/Kalkyle/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Kalkyle\Http\Controllers\KalkyleController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('kalkyles', KalkyleController::class)->names('kalkyle');
-});
